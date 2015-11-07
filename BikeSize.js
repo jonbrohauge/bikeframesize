5e8758b53560156e2989300c6dea173e5bcecf2f
Jon Brohauge <jon@brohauge.dk>
2015-11-07 19:10:03 +0100

    Add Javascript functionality refactor

--- BikeSize.js
+++ BikeSize.js
@@ -1,20 +1,20 @@
-<script language="JavaScript">
-var minFrameSize = 65,
-    maxFrameSize = 105;
+function loadForm() {
+    var minFrameSize = 65,
+        maxFrameSize = 105;
 
-var myDiv = document.getElementById("myDiv");
+    var frameSizeDiv = document.getElementById("frameSizeDiv");
 
 //Create and append select list
-var selectList = document.createElement("select");
-selectList.setAttribute("id", "mySelect");
-myDiv.appendChild(selectList);
+    var selectList = document.createElement("select");
+    selectList.setAttribute("id", "mySelect");
+    frameSizeDiv.appendChild(selectList);
 
 //Create and append the options
-for (var i = minFrameSize; i < maxFrameSize; i++) {
-    var option = document.createElement("option");
-    option.setAttribute("value", i);
-    option.text = i;
-    selectList.appendChild(option);
-}
+    for (var i = minFrameSize; i < maxFrameSize; i++) {
+        var option = document.createElement("option");
+        option.setAttribute("value", i);
+        option.text = i;
+        selectList.appendChild(option);
+    }
 
-</script>
+}
